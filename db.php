da326f7558a72b40eb36f54913fc05354d76732c
edongjoselito <143322244+edongjoselito@users.noreply.github.com>
2026-02-24 21:43:12 +0800

    c

--- db.php
+++ db.php
@@ -4,7 +4,7 @@
 $dbHost = '127.0.0.1';
 $dbName = 'xx_module_b';
 $dbUser = 'root';
-$dbPass = 'moth34board';
+$dbPass = '';
 $dbCharset = 'utf8mb4';
 
 $dsn = "mysql:host=$dbHost;dbname=$dbName;charset=$dbCharset";
